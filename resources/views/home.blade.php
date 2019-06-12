781403a8557723811052c401e6a340fa023a535e
Max Santosa <max186132@gmail.com>
2019-06-12 10:26:04 +0800

    Added partials

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,99 +1,10 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        
-        <!-- Bootstrap -->
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-
-        <!-- jQuery library -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-
-        <!-- Popper JS -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-
-        <!-- Latest compiled JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-        
-        <!-- JQuery Tables -->
-        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.css">
-  
-        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        @include('partials.head')
     </head>
     <body>
-        <nav class="navbar navbar-expand-lg sticky-top" style="background-color: #99c2ff">
-            <div class="container-fluid">
-                <ul class="navbar-nav navbar-left">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="/" style="color:#cc5200">EMP</a>
-                    </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="/list" style="color:#0052cc">EMPLOYEE LIST</a>
-                    </li>
-                </ul>
-            </div>
-        </nav> 
+        @include('partials.navbar')
         <br>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,12 @@
+<nav class="navbar navbar-expand-lg sticky-top" style="background-color: #99c2ff">
+    <div class="container-fluid">
+        <ul class="navbar-nav navbar-left">
+            <li class="nav-item active">
+                <a class="nav-link" href="/" style="color:#cc5200">EMP</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="/list" style="color:#0052cc">EMPLOYEE LIST</a>
+            </li>
+        </ul>
+    </div>
+</nav>
