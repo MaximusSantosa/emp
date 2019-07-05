306d518a7cb8c4a4291ca43137357c8afe2ba727
juliussantosa <juliussantosa@gmail.com>
2019-07-05 17:58:09 +0000

    update shell and mysql

--- public/docs/phpcli/refresh.sh
+++ public/docs/phpcli/refresh.sh
@@ -1,3 +1,3 @@
-host="emp.cnpzdpbr28zx.ap-southeast-1.rds.amazonaws.com"
+host="jaysg.cqby2okadb1u.ap-southeast-1.rds.amazonaws.com"
 wget --no-cache --no-cookies -O /tmp/combine.csv "https://docs.google.com/spreadsheets/d/1R2QTK1QccFAKVbCUbwgwnqmR2S0Xo7odoMqcjhKXBmo/pub?gid=810227757&single=true&output=csv"
-mysql --local-infile -A -umax -pbandung123 emp -h $host < /var/www/html/emp/public/docs/phpcli/loadData.sql
+mysql --local-infile -A -ucf_admin -pBapintri123 jay -h $host < /var/www/html/emp/public/docs/phpcli/loadData.sql
